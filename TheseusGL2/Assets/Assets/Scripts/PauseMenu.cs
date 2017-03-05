202e89b860c730a32f054245c25b018670fa8bd8
Gaming_Laptop\Enra <arnevanommen@outlook.com>
2017-03-05 14:07:49 +0100

    MainMenu and PauseMenu Progress

---
+++ TheseusGL2/Assets/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    GameObject pauseMenuPanel;
+    
+
+    void Awake ()
+    {
+        //pausemenu off
+    }
+    void Update ()
+    {
+        if (Input.GetButtonDown("Escape"))
+        {
+            //turn pausemenu on
+            Time.timeScale = 0;
+        }
+    }
+    public void ResumeGame ()
+    {
+        //turn pausemenu off
+        Time.timeScale = 1;
+    }
+    public void Settings ()
+    {
+        //turn pausemenu off
+        
+    }
+    public void MainMenu ()
+    {
+        //turn pausemenu off
+        SceneManager.LoadScene("ArneScene");
+        Time.timeScale = 1;
+    }
+}
