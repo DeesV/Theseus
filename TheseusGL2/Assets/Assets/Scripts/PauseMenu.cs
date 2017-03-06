5c6062ebe6ca0d5c214bd07f007eb3f4692c91e2
Gaming_Laptop\Enra <arnevanommen@outlook.com>
2017-03-06 15:48:03 +0100

    PauseMenu Progress

--- TheseusGL2/Assets/Assets/Scripts/PauseMenu.cs
+++ TheseusGL2/Assets/Assets/Scripts/PauseMenu.cs
@@ -7,25 +7,35 @@ public class PauseMenu : MonoBehaviour {
 
     public GameObject pauseMenuPanel;
     public GameObject hudPanel;
+    public GameObject settingsMenuPanel;
+    public GameObject mainMenuPanel;            //if you press main menu
 
 
     void Awake ()
     {
+        mainMenuPanel.SetActive(false);
         pauseMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(false);
+        
     }
     void Update ()
     {
         if (Input.GetButtonDown("Escape"))
         {
-            if (pauseMenuPanel.activeSelf)
+            if (!pauseMenuPanel.activeSelf)
+            {
+                pauseMenuPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else if (pauseMenuPanel.activeSelf)
             {
                 pauseMenuPanel.SetActive(false);
                 Time.timeScale = 1;
             }
-            else
+            if (mainMenuPanel.activeSelf)
             {
+                mainMenuPanel.SetActive(false);
                 pauseMenuPanel.SetActive(true);
-                Time.timeScale = 0;
             }
         }
     }
@@ -37,11 +47,22 @@ public class PauseMenu : MonoBehaviour {
     public void Settings ()
     {
         pauseMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(true);
+        Time.timeScale = 0;
     }
     public void MainMenu ()
     {
+        mainMenuPanel.SetActive(true);
         pauseMenuPanel.SetActive(false);
+    }
+    public void MainMenuYes ()
+    {
         SceneManager.LoadScene("ArneScene");
         Time.timeScale = 1;
+    }
+    public void MainMenuNo ()
+    {
+        mainMenuPanel.SetActive(false);
+        pauseMenuPanel.SetActive(true);
     }
 }
