c245d27b94427bb1314f3fd8d3b96498a6675fc0
Gaming_Laptop\Enra <arnevanommen@outlook.com>
2017-03-08 11:43:04 +0100

    MainMenu Progress
    Making some progress onece again!

--- TheseusGL2/Assets/Assets/Scripts/MainMenu.cs
+++ TheseusGL2/Assets/Assets/Scripts/MainMenu.cs
@@ -5,22 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    //public GameObject bgImagePanel;
-    //public GameObject titleScreenPanel;
-    //public GameObject pressEnterPanel;
-    
+    public GameObject bgImagePanel;
+    public GameObject titleScreenPanel;
+    public GameObject pressEnterPanel;
+
+    public GameObject settingsPanel;
+    public GameObject creditPanel;
+
     //Only needed when I make a complete MainMenu with options etc
-    /*void Awake ()
+    void Awake ()
     {
         bgImagePanel.SetActive(true);
         titleScreenPanel.SetActive(true);
-        pressEnterPanel.SetActive(true);
-    }*/
+        //pressEnterPanel.SetActive(true);
+
+        settingsPanel.SetActive(false);
+        creditPanel.SetActive(false);
+    }
 	void Update ()
     {
-        if (Input.GetButtonDown("Enter"))
+        if(Input.GetButtonDown("Escape"))
+        {
+            if (settingsPanel.activeSelf)
+            {
+                settingsPanel.SetActive(false);
+            }
+            if (creditPanel.activeSelf)
+            {
+                creditPanel.SetActive(false);
+            }
+        }//to be able to press enter to play
+        /*if (Input.GetButtonDown("Enter"))
         {
             SceneManager.LoadScene("ArneScene2");
-        }
+        }*/
 	}
+    public void StartGame ()
+    {
+        SceneManager.LoadScene("ArneScene2");
+    }
+    public void Settings ()
+    {
+        settingsPanel.SetActive(true);
+    }
+    public void Credits ()
+    {
+        creditPanel.SetActive(true);
+    }
+    public void ExitGame ()
+    {
+        Application.Quit();
+    }
 }
