0b7d60afdf0b1cf6ef855c0c96f6ba76366eab4a
Gaming_Laptop\Enra <arnevanommen@outlook.com>
2017-03-11 16:52:46 +0100

    SettingsMenu Progress
    It's coming along!

--- TheseusGL2/Assets/Assets/Scripts/SettingsMenu.cs
+++ TheseusGL2/Assets/Assets/Scripts/SettingsMenu.cs
@@ -9,10 +9,23 @@ public class SettingsMenu : MonoBehaviour {
     public GameObject settingsMenuPanel;
     public GameObject mainMenuPanel;
 
+    public GameObject soundOptionsPanel;
+    public GameObject keyBindingsPanel;
+    public GameObject controlSettingsPanel;
+    public GameObject gameSettingsPanel;
 
+    void Awake ()
+    {
+        settingsMenuPanel.SetActive(false);
+        soundOptionsPanel.SetActive(false);
+        keyBindingsPanel.SetActive(false);
+        controlSettingsPanel.SetActive(false);
+        gameSettingsPanel.SetActive(false);
+    }
     public void SettingsBack ()
     {
         settingsMenuPanel.SetActive(false);
+        pauseMenuPanel.SetActive(true);
     }
     public void Sound ()
     {
