e1a0b8a1162abdadf3a30c2a4e06093e84bd721d
Gaming_Laptop\Enra <arnevanommen@outlook.com>
2017-03-23 13:04:20 +0100

    DontDestroyScript

---
+++ TheseusGL2/Assets/ArneStuff/DontDestroyOnLoad.cs
@@ -0,0 +1,23 @@
+﻿//DontDestroyOnLoad by Jordi
+
+using UnityEngine;
+using System.Collections;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    public static DontDestroyOnLoad Instance;
+
+    //Adds DontDestroyOnLoad on GameObject. If object already exists delete to prevent having two
+    void Awake()
+    {
+        if (Instance)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+    }
+}
